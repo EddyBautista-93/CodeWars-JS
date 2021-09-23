ad6e8309ec584f6219ba7878de4e96caff2526a3
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-23 05:32:44 -0500

    add completed q

--- incrementString.js
+++ incrementString.js
@@ -15,5 +15,19 @@
 // foo099 -> foo100
 
 const incrementString = (strng) => {
+  var string = strng.replace(/[0-9]/g, '');
+  
+  var len = strng.length  - string.length;
+  
+  var str = strng.slice(string.length) || "0";
+  
+  str = (parseInt(str) + 1).toString();
+  
+  while (str.length < len) {
+    str = "0" + str;
+  }
     
+ return string.concat(str);
 }
+
+incrementString("foo000");
